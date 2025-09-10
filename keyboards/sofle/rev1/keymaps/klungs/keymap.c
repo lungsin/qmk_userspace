a8f18da35332828f186f16ec2c75759ce3225ac1
Kwee Lung Sin <klungs.dev@gmail.com>
2025-09-11 02:40:15 +0700

    symbol: swap backslash and pipe key

--- keyboards/sofle/rev1/keymaps/klungs/keymap.c
+++ keyboards/sofle/rev1/keymaps/klungs/keymap.c
@@ -48,9 +48,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //        ┌─────┬────┬─────┬─────┬─────┬─────┐               ┌─────────────┬─────┬─────┬─────┬─────┬─────┐
 //        │ f12 │ f1 │ f2  │ f3  │ f4  │ f5  │               │     f6      │ f7  │ f8  │ f9  │ f10 │ f11 │
 //        ├─────┼────┼─────┼─────┼─────┼─────┤               ├─────────────┼─────┼─────┼─────┼─────┼─────┤
-//        │  `  │ 1  │  2  │  3  │  4  │  5  │               │      6      │  7  │  8  │  9  │  0  │  \  │
+//        │  `  │ 1  │  2  │  3  │  4  │  5  │               │      6      │  7  │  8  │  9  │  0  │  |  │
 //        ├─────┼────┼─────┼─────┼─────┼─────┤               ├─────────────┼─────┼─────┼─────┼─────┼─────┤
-//        │  ~  │ !  │  @  │  #  │  $  │  %  │               │      ^      │  &  │  *  │  (  │  )  │  |  │
+//        │  ~  │ !  │  @  │  #  │  $  │  %  │               │      ^      │  &  │  *  │  (  │  )  │  \  │
 //        ├─────┼────┼─────┼─────┼─────┼─────┼─────┐   ┌─────┼─────────────┼─────┼─────┼─────┼─────┼─────┤
 //        │     │ [  │  {  │  =  │  _  │  <  │     │   │     │      >      │  +  │  -  │  }  │  ]  │     │
 //        └─────┴────┼─────┼─────┼─────┼─────┼─────┤   ├─────┼─────────────┼─────┼─────┼─────┼─────┴─────┘
@@ -58,8 +58,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //                   └─────┴─────┴─────┴─────┴─────┘   └─────┴─────────────┴─────┴─────┴─────┘
 [_SYMBOL] = LAYOUT(
       KC_F12  , KC_F1   , KC_F2   , KC_F3   , KC_F4   , KC_F5   ,                         KC_F6       , KC_F7   , KC_F8   , KC_F9   , KC_F10  , KC_F11 ,
-      KC_GRV  , KC_1    , KC_2    , KC_3    , KC_4    , KC_5    ,                         KC_6        , KC_7    , KC_8    , KC_9    , KC_0    , KC_BSLS,
-      KC_TILD , KC_EXLM , KC_AT   , KC_HASH , KC_DLR  , KC_PERC ,                         KC_CIRC     , KC_AMPR , KC_ASTR , KC_LPRN , KC_RPRN , KC_PIPE,
+      KC_GRV  , KC_1    , KC_2    , KC_3    , KC_4    , KC_5    ,                         KC_6        , KC_7    , KC_8    , KC_9    , KC_0    , KC_PIPE,
+      KC_TILD , KC_EXLM , KC_AT   , KC_HASH , KC_DLR  , KC_PERC ,                         KC_CIRC     , KC_AMPR , KC_ASTR , KC_LPRN , KC_RPRN , KC_BSLS,
       KC_TRNS , KC_LBRC , KC_LCBR , KC_EQL  , KC_UNDS , KC_LT   , KC_TRNS ,     KC_TRNS , KC_GT       , KC_PLUS , KC_MINS , KC_RCBR , KC_RBRC , KC_TRNS,
                           KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS ,     KC_TRNS , MO(_NUMPAD) , KC_TRNS , KC_TRNS , KC_TRNS
 ),
