72ee6192f30554a1d7df173a44475b8d8f1d0871
Kwee Lung Sin <klungs.dev@gmail.com>
2025-12-24 23:08:04 +0700

    feat: setup more enter keys and swap backslash with pipe

--- keyboards/sofle/rev1/keymaps/klungs/keymap.c
+++ keyboards/sofle/rev1/keymaps/klungs/keymap.c
@@ -48,9 +48,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //        ┌─────┬────┬─────┬─────┬─────┬─────┐               ┌─────────────┬─────┬─────┬─────┬─────┬─────┐
 //        │ f12 │ f1 │ f2  │ f3  │ f4  │ f5  │               │     f6      │ f7  │ f8  │ f9  │ f10 │ f11 │
 //        ├─────┼────┼─────┼─────┼─────┼─────┤               ├─────────────┼─────┼─────┼─────┼─────┼─────┤
-//        │  `  │ 1  │  2  │  3  │  4  │  5  │               │      6      │  7  │  8  │  9  │  0  │  |  │
+//        │  `  │ 1  │  2  │  3  │  4  │  5  │               │      6      │  7  │  8  │  9  │  0  │  \  │
 //        ├─────┼────┼─────┼─────┼─────┼─────┤               ├─────────────┼─────┼─────┼─────┼─────┼─────┤
-//        │  ~  │ !  │  @  │  #  │  $  │  %  │               │      ^      │  &  │  *  │  (  │  )  │  \  │
+//        │  ~  │ !  │  @  │  #  │  $  │  %  │               │      ^      │  &  │  *  │  (  │  )  │  |  │
 //        ├─────┼────┼─────┼─────┼─────┼─────┼─────┐   ┌─────┼─────────────┼─────┼─────┼─────┼─────┼─────┤
 //        │     │ [  │  {  │  =  │  _  │  <  │     │   │     │      >      │  +  │  -  │  }  │  ]  │     │
 //        └─────┴────┼─────┼─────┼─────┼─────┼─────┤   ├─────┼─────────────┼─────┼─────┼─────┼─────┴─────┘
@@ -58,29 +58,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //                   └─────┴─────┴─────┴─────┴─────┘   └─────┴─────────────┴─────┴─────┴─────┘
 [_SYMBOL] = LAYOUT(
       KC_F12  , KC_F1   , KC_F2   , KC_F3   , KC_F4   , KC_F5   ,                         KC_F6       , KC_F7   , KC_F8   , KC_F9   , KC_F10  , KC_F11 ,
-      KC_GRV  , KC_1    , KC_2    , KC_3    , KC_4    , KC_5    ,                         KC_6        , KC_7    , KC_8    , KC_9    , KC_0    , KC_PIPE,
-      KC_TILD , KC_EXLM , KC_AT   , KC_HASH , KC_DLR  , KC_PERC ,                         KC_CIRC     , KC_AMPR , KC_ASTR , KC_LPRN , KC_RPRN , KC_BSLS,
+      KC_GRV  , KC_1    , KC_2    , KC_3    , KC_4    , KC_5    ,                         KC_6        , KC_7    , KC_8    , KC_9    , KC_0    , KC_BSLS,
+      KC_TILD , KC_EXLM , KC_AT   , KC_HASH , KC_DLR  , KC_PERC ,                         KC_CIRC     , KC_AMPR , KC_ASTR , KC_LPRN , KC_RPRN , KC_PIPE,
       KC_TRNS , KC_LBRC , KC_LCBR , KC_EQL  , KC_UNDS , KC_LT   , KC_TRNS ,     KC_TRNS , KC_GT       , KC_PLUS , KC_MINS , KC_RCBR , KC_RBRC , KC_TRNS,
                           KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS ,     KC_TRNS , MO(_NUMPAD) , KC_TRNS , KC_TRNS , KC_TRNS
 ),
 
-//        ┌──────┬───────────────┬───────────────┬───────────────┬───────────────┬───────────────┐               ┌──────┬────────────┬──────┬────────────┬──────┬─────┐
-//        │      │               │               │               │               │               │               │      │            │      │            │      │     │
-//        ├──────┼───────────────┼───────────────┼───────────────┼───────────────┼───────────────┤               ├──────┼────────────┼──────┼────────────┼──────┼─────┤
-//        │      │      esc      │  LALT(left)   │    LCTL(f)    │  LALT(rght)   │      ins      │               │ pgup │ LCTL(left) │  up  │ LCTL(rght) │ bspc │  |  │
-//        ├──────┼───────────────┼───────────────┼───────────────┼───────────────┼───────────────┤               ├──────┼────────────┼──────┼────────────┼──────┼─────┤
-//        │ caps │ OSM(MOD_LSFT) │ OSM(MOD_LGUI) │ OSM(MOD_LALT) │ OSM(MOD_LCTL) │ OSM(MOD_RALT) │               │ pgdn │    left    │ down │    rght    │ ent  │  \  │
-//        ├──────┼───────────────┼───────────────┼───────────────┼───────────────┼───────────────┼─────┐   ┌─────┼──────┼────────────┼──────┼────────────┼──────┼─────┤
-//        │      │    LCTL(z)    │    LCTL(x)    │    LCTL(c)    │    LCTL(v)    │     lgui      │     │   │     │ caps │    home    │ pscr │    end     │ del  │     │
-//        └──────┴───────────────┼───────────────┼───────────────┼───────────────┼───────────────┼─────┤   ├─────┼──────┼────────────┼──────┼────────────┼──────┴─────┘
-//                               │               │               │               │  MO(_NUMPAD)  │ spc │   │     │      │            │      │            │
-//                               └───────────────┴───────────────┴───────────────┴───────────────┴─────┘   └─────┴──────┴────────────┴──────┴────────────┘
+//        ┌──────┬───────────────┬───────────────┬───────────────┬───────────────┬─────────────┐               ┌──────┬────────────┬──────┬────────────┬─────┬─────┐
+//        │      │               │               │               │               │             │               │      │            │      │            │     │     │
+//        ├──────┼───────────────┼───────────────┼───────────────┼───────────────┼─────────────┤               ├──────┼────────────┼──────┼────────────┼─────┼─────┤
+//        │      │      esc      │  LALT(left)   │               │  LALT(rght)   │     ins     │               │ pgup │ LCTL(left) │  up  │ LCTL(rght) │ del │  \  │
+//        ├──────┼───────────────┼───────────────┼───────────────┼───────────────┼─────────────┤               ├──────┼────────────┼──────┼────────────┼─────┼─────┤
+//        │ caps │ OSM(MOD_LSFT) │ OSM(MOD_LGUI) │ OSM(MOD_LALT) │ OSM(MOD_LCTL) │    lgui     │               │ pgdn │    left    │ down │    rght    │ ent │  |  │
+//        ├──────┼───────────────┼───────────────┼───────────────┼───────────────┼─────────────┼─────┐   ┌─────┼──────┼────────────┼──────┼────────────┼─────┼─────┤
+//        │      │      ent      │      ent      │      ent      │      ent      │    pscr     │     │   │     │ caps │    home    │ ent  │    end     │ ent │     │
+//        └──────┴───────────────┼───────────────┼───────────────┼───────────────┼─────────────┼─────┤   ├─────┼──────┼────────────┼──────┼────────────┼─────┴─────┘
+//                               │               │               │               │ MO(_NUMPAD) │ spc │   │     │      │            │      │            │
+//                               └───────────────┴───────────────┴───────────────┴─────────────┴─────┘   └─────┴──────┴────────────┴──────┴────────────┘
 [_NAVIGATION] = LAYOUT(
-      KC_TRNS , KC_TRNS       , KC_TRNS       , KC_TRNS       , KC_TRNS       , KC_TRNS       ,                         KC_TRNS , KC_TRNS       , KC_TRNS , KC_TRNS       , KC_TRNS , KC_TRNS,
-      KC_TRNS , KC_ESC        , LALT(KC_LEFT) , LCTL(KC_F)    , LALT(KC_RGHT) , KC_INS        ,                         KC_PGUP , LCTL(KC_LEFT) , KC_UP   , LCTL(KC_RGHT) , KC_BSPC , KC_PIPE,
-      KC_CAPS , OSM(MOD_LSFT) , OSM(MOD_LGUI) , OSM(MOD_LALT) , OSM(MOD_LCTL) , OSM(MOD_RALT) ,                         KC_PGDN , KC_LEFT       , KC_DOWN , KC_RGHT       , KC_ENT  , KC_BSLS,
-      KC_TRNS , LCTL(KC_Z)    , LCTL(KC_X)    , LCTL(KC_C)    , LCTL(KC_V)    , KC_LGUI       , KC_TRNS ,     KC_TRNS , KC_CAPS , KC_HOME       , KC_PSCR , KC_END        , KC_DEL  , KC_TRNS,
-                                KC_TRNS       , KC_TRNS       , KC_TRNS       , MO(_NUMPAD)   , KC_SPC  ,     KC_TRNS , KC_TRNS , KC_TRNS       , KC_TRNS , KC_TRNS
+      KC_TRNS , KC_TRNS       , KC_TRNS       , KC_TRNS       , KC_TRNS       , KC_TRNS     ,                         KC_TRNS , KC_TRNS       , KC_TRNS , KC_TRNS       , KC_TRNS , KC_TRNS,
+      KC_TRNS , KC_ESC        , LALT(KC_LEFT) , KC_TRNS       , LALT(KC_RGHT) , KC_INS      ,                         KC_PGUP , LCTL(KC_LEFT) , KC_UP   , LCTL(KC_RGHT) , KC_DEL  , KC_BSLS,
+      KC_CAPS , OSM(MOD_LSFT) , OSM(MOD_LGUI) , OSM(MOD_LALT) , OSM(MOD_LCTL) , KC_LGUI     ,                         KC_PGDN , KC_LEFT       , KC_DOWN , KC_RGHT       , KC_ENT  , KC_PIPE,
+      KC_TRNS , KC_ENTER      , KC_ENTER      , KC_ENTER      , KC_ENTER      , KC_PSCR     , KC_TRNS ,     KC_TRNS , KC_CAPS , KC_HOME       , KC_ENT  , KC_END        , KC_ENT  , KC_TRNS,
+                                KC_TRNS       , KC_TRNS       , KC_TRNS       , MO(_NUMPAD) , KC_SPC  ,     KC_TRNS , KC_TRNS , KC_TRNS       , KC_TRNS , KC_TRNS
 ),
 
 //        ┌─────────┬───────────────┬────────────────┬───────────────┬───────────────┬───────────────┐               ┌─────┬─────┬─────┬─────┬──────┬─────────┐
