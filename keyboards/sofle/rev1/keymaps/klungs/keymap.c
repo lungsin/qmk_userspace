8e1a5d9b8b794a5c8e6bd1abba89422d23e2805f
Kwee Lung Sin <klungs.dev@gmail.com>
2026-05-10 22:31:52 +0700

    tweak symbol layer the brackets

--- keyboards/sofle/rev1/keymaps/klungs/keymap.c
+++ keyboards/sofle/rev1/keymaps/klungs/keymap.c
@@ -1,6 +1,7 @@
 // Copyright 2023 QMK
 // SPDX-License-Identifier: GPL-2.0-or-later
 #include "keycodes.h"
+#include "keymap_us.h"
 #include QMK_KEYBOARD_H
 
 #include "encoder.c"
@@ -52,16 +53,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //        ├──────┼────┼─────┼─────┼─────┼─────┤               ├─────────────┼─────┼─────┼─────┼─────┼─────┤
 //        │  ~   │ !  │  @  │  #  │  $  │  %  │               │      ^      │  &  │  *  │  (  │  )  │  ?  │
 //        ├──────┼────┼─────┼─────┼─────┼─────┼─────┐   ┌─────┼─────────────┼─────┼─────┼─────┼─────┼─────┤
-//        │ lctl │ [  │  {  │  =  │  _  │  <  │     │   │     │      >      │  +  │  -  │  }  │  ]  │  |  │
+//        │ lctl │ <  │  {  │  [  │  =  │  _  │     │   │     │      -      │  +  │  ]  │  }  │  >  │  |  │
 //        └──────┴────┼─────┼─────┼─────┼─────┼─────┤   ├─────┼─────────────┼─────┼─────┼─────┼─────┴─────┘
 //                    │     │     │     │     │     │   │     │ MO(_NUMPAD) │     │     │     │
 //                    └─────┴─────┴─────┴─────┴─────┘   └─────┴─────────────┴─────┴─────┴─────┘
 [_SYMBOL] = LAYOUT(
-      KC_F12  , KC_F1   , KC_F2   , KC_F3   , KC_F4   , KC_F5   ,                         KC_F6       , KC_F7   , KC_F8   , KC_F9   , KC_F10  , KC_F11     ,
-      KC_GRV  , KC_1    , KC_2    , KC_3    , KC_4    , KC_5    ,                         KC_6        , KC_7    , KC_8    , KC_9    , KC_0    , KC_TRNS    ,
-      KC_TILD , KC_EXLM , KC_AT   , KC_HASH , KC_DLR  , KC_PERC ,                         KC_CIRC     , KC_AMPR , KC_ASTR , KC_LPRN , KC_RPRN , KC_QUESTION,
-      KC_LCTL , KC_LBRC , KC_LCBR , KC_EQL  , KC_UNDS , KC_LT   , KC_TRNS ,     KC_TRNS , KC_GT       , KC_PLUS , KC_MINS , KC_RCBR , KC_RBRC , KC_PIPE    ,
-                          KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS ,     KC_TRNS , MO(_NUMPAD) , KC_TRNS , KC_TRNS , KC_TRNS
+      KC_F12  , KC_F1   , KC_F2   , KC_F3   , KC_F4    , KC_F5         ,                         KC_F6       , KC_F7   , KC_F8   , KC_F9   , KC_F10  , KC_F11     ,
+      KC_GRV  , KC_1    , KC_2    , KC_3    , KC_4     , KC_5          ,                         KC_6        , KC_7    , KC_8    , KC_9    , KC_0    , KC_TRNS    ,
+      KC_TILD , KC_EXLM , KC_AT   , KC_HASH , KC_DLR   , KC_PERC       ,                         KC_CIRC     , KC_AMPR , KC_ASTR , KC_LPRN , KC_RPRN , KC_QUESTION,
+      KC_LCTL , KC_LT   , KC_LCBR , KC_LBRC , KC_EQUAL , KC_UNDERSCORE , KC_TRNS ,     KC_TRNS , KC_MINS     , KC_PLUS , KC_RBRC , KC_RCBR , KC_GT   , KC_PIPE    ,
+                          KC_TRNS , KC_TRNS , KC_TRNS  , KC_TRNS       , KC_TRNS ,     KC_TRNS , MO(_NUMPAD) , KC_TRNS , KC_TRNS , KC_TRNS
 ),
 
 //        ┌─────┬───────────────┬───────────────┬───────────────┬───────────────┬─────────────┐               ┌──────┬──────┬──────┬──────┬──────┬─────┐
